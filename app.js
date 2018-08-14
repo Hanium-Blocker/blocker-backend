0ac63e61dacc9c7c7039ed04f2a61112b4efe731
jo-bata <jobata@skuniv.ac.kr>
2018-08-14 17:37:44 +0900

    feat: Initialize App

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const auth = require('./routes/auth');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/auth/', auth);
+
+module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+// const conn = require('../models/mysql');
+// const config = require('../config/config');
+
+// router.get('/', (req, res) => {
+
+// });
+
+module.exports = router;
