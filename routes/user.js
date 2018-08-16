28cfd2f413b7dc38bfa9091c4635e4037723f816
jo-bata <jobata@skuniv.ac.kr>
2018-08-17 01:15:33 +0900

    feat: Add check all candiate api

--- routes/user.js
+++ routes/user.js
@@ -16,4 +16,17 @@ router.get('/election', (req, res) => {
   });
 });
 
+router.get('/election/:electionId/candidate', (req, res) => {
+  const sql = 'SELECT * FROM candidates WHERE election_id=?';
+  conn.query(sql, [req.params.electionId], (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json(config.status.sc500);
+    } else {
+      console.log(`Response ${req.params.electionId}'s All candidate !`);
+      res.json(results);
+    }
+  });
+});
+
 module.exports = router;
