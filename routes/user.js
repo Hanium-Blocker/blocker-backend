a0cac0f79b002b53ceab448d397924be0f31dd11
jo-bata <jobata@skuniv.ac.kr>
2018-08-30 03:23:41 +0900

    feat: Add update a single election API

--- routes/user.js
+++ routes/user.js
@@ -43,6 +43,30 @@ router.post('/election', (req, res) => {
   });
 });
 
+router.put('/election/:electionId', (req, res) => {
+  const sql = 'SELECT COUNT(*) count FROM elections WHERE name=?';
+  conn.query(sql, [req.body.name], (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json(config.status.sc500);
+    } else if (results[0].count > 0) {
+      console.log('Duplicate election !');
+      res.status(200).json(config.status.sc409);
+    } else {
+      const sql = 'UPDATE elections SET name=? WHERE id=?';
+      conn.query(sql, [req.body.name, req.params.electionId], (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json(config.status.sc500);
+        } else {
+          console.log('Update a single election !');
+          res.status(200).json(config.status.sc200);
+        }
+      });
+    }
+  });
+});
+
 router.get('/election/:electionId/candidate', (req, res) => {
   const sql = 'SELECT * FROM candidates WHERE election_id=?';
   conn.query(sql, [req.params.electionId], (err, results) => {
